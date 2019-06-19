77058bdd423345c522c0db59836de2d7695c3a73
victorN <victornguyen082@gmail.com>
2019-06-19 18:42:39 -0400

    control and dashboard finished

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+
+import React from "react";
+import renderer from "react-test-renderer"; // 1: install this npm module as a dev dependency
+import { render, fireEvent } from "react-testing-library";
+
+  import Dashboard from "./Dashboard";
+describe("<Dashboard/>", () => {
+  it("renders test", () => {
+
+     render(<Dashboard />);
+  });
+  it("snapshot match", () => {
+    const tree = renderer.create(<Dashboard />); 
+
+     expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
